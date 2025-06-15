e65c5232800d024ca20297a33a87ed9b5b16930e
Asher payn <asherpayn@gmail.com>
2025-06-15 21:39:23 +0100

    stuff

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+APP = ['Artwork Downloader.py']  # Replace with the name of your Python script
+DATA_FILES = []
+OPTIONS = {
+	'argv_emulation': True,
+	'packages': ['requests', 'spotipy', 'PIL'],  # Include necessary packages
+	'iconfile': 'icon.icon',  # Optional: path to your app icon
+}
+
+setup(
+	app=APP,
+	data_files=DATA_FILES,
+	options={'py2app': OPTIONS},
+	setup_requires=['py2app'],
+)
